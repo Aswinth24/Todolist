a19a54c3d9033b8c2ea8b4b7c155cd1ede886a37
Aswinth K <aswinth24@gmail.com>
2023-07-14 21:38:11 +0530

    initial commit

---
+++ src/projects.js
@@ -0,0 +1,21 @@
+export class Project
+{
+    constructor(projectTitle,totalTask)
+    {
+    this.projectTitle=projectTitle;
+    this.totalTask=totalTask;
+    }
+    getProjectTitle=()=>{
+        return this. projectTitle;
+    }
+    getTotalTask=()=>{
+        return this.totalTask;
+    }
+    setTotalTask=(count)=>{
+        this.totalTask=count;
+    }
+    setProjectTitle=(title)=>{
+        this.projectTitle=title;
+    }
+
+  }
---
+++ src/taskInfo.js
@@ -0,0 +1,41 @@
+export class TaskInfo{
+    constructor(taskTitle,projectTitle,description,priority,date)
+    {
+        this.taskTitle=taskTitle;
+        this.projectTitle=projectTitle;
+        this.description=description;
+        this.priority=priority;
+        this.date=date;
+    }
+     getTaskTitle=()=>{
+        return this.taskTitle;
+    }
+     getProjectTitle=()=>{
+        return this.projectTitle;
+    }
+     getDescription=()=>{
+        return this.description;
+    }
+     getPriority=()=>{
+        return this.priority;
+    }
+     getDate=()=>{
+        return this.date;
+    }
+     setTaskTitle=(taskTitle)=>{
+        this.taskTitle=taskTitle;
+    }
+     setProjectTitle=(projectTitle)=>{
+        this.projectTitle=projectTitle;
+    }
+     setDescription=(description)=>{
+        this.description=description;
+    }
+     setPriority=(priority)=>{
+        this.priority=priority;
+    }
+     setDate=(date)=>{
+        this.date=date;
+    }
+   
+}
